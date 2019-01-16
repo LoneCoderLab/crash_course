f1a0867b9ac4d6fe79215265cccd89a5974ca187
LoneCoderLab <joe@lonecoderlab.com>
2019-01-16 13:53:54 +0100

    0.0.7 Fetch data from JSON API, converted to StatefulWidgets

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -1,32 +1,48 @@
 import 'package:flutter/material.dart';
 import 'models/location.dart';
 import 'styles.dart';
-import 'mocks/mock_location.dart';
 
-class LocationDetail extends StatelessWidget {
+class LocationDetail extends StatefulWidget {
   final int locationID;
 
   LocationDetail(this.locationID);
 
   @override
+  createState() => _LocationDetailState(this.locationID);
+}
+
+class _LocationDetailState extends State<LocationDetail> {
+  final int locationID;
+  Location location = Location.blank();
+
+  _LocationDetailState(this.locationID);
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var location = MockLocation.fetch(this.locationID);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          location.name,
-          style: Styles.navBarTitle,
-        ),
-      ),
-      body: SingleChildScrollView(
-        child:
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: _renderBody(context, location),
-          ),
-      ),
-    );
+        appBar: AppBar(title: Text(location.name, style: Styles.navBarTitle)),
+        body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: _renderBody(context, location),
+            )));
+  }
+
+  loadData() async {
+    final location = await Location.fetchByID(this.locationID);
+
+    if (mounted) {
+      setState(() {
+        this.location = location;
+      });
+    }
   }
 
   List<Widget> _renderBody(BuildContext context, Location location) {
@@ -45,30 +61,31 @@ class LocationDetail extends StatelessWidget {
     return result;
   }
 
-  Widget _sectionTitle(String title) {
+  Widget _sectionTitle(String text) {
     return Container(
-      padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
-      child: Text(
-        title,
-        textAlign: TextAlign.left,
-        style: Styles.headerLarge,
-      ),
-    );
+        padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
+        child:
+        Text(text, textAlign: TextAlign.left, style: Styles.headerLarge));
   }
 
   Widget _sectionText(String text) {
     return Container(
         padding: EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 15.0),
-        child: Text(
-          text,
-          style: Styles.textDefault,
-        ));
+        child: Text(text, style: Styles.textDefault));
   }
 
   Widget _bannerImage(String url, double height) {
+    Image image;
+    try {
+      if (url.isNotEmpty) {
+        image = Image.network(url, fit: BoxFit.fitWidth);
+      }
+    } catch (e) {
+      print("could not load image $url");
+    }
     return Container(
       constraints: BoxConstraints.tightFor(height: height),
-      child: Image.network(url, fit: BoxFit.fitWidth),
+      child: image,
     );
   }
 }
--- test/unit/location_test.dart
+++ test/unit/location_test.dart
@@ -2,27 +2,42 @@ import 'dart:convert';
 import 'package:test_api/test_api.dart';
 import '../../lib/models/location.dart';
 
+//void main() {
+//  test('test location deserialization', () {
+//    const locationJSON =
+//        '{ '
+//        '"name": "Arashiyama Bamboo Grove", '
+//        '"url": "https://cdn-images-1.medium.com/max/2000/1*vdJuSUKWl_SA9Lp-32ebnA.jpeg", '
+//        '"facts": [{ '
+//        '"title": "Summary", '
+//        '"text": "This bamboo grove is on the outskirts of Kyoto." }] '
+//        '}';
+//    final locationMap = json.decode(locationJSON) as Map<String, dynamic>;
+//
+//    expect('Arashiyama Bamboo Grove', equals(locationMap['name']));
+//
+//    final location = Location.fromJson(locationMap);
+//
+//    expect(location.name, equals(locationMap['name']));
+//    expect(location.url, equals(locationMap['url']));
+//
+//    expect(location.facts[0].title, matches(locationMap['facts'][0]['title']));
+//    expect(location.facts[0].text, matches(locationMap['facts'][0]['text']));
+//
+//  });
+//}
+
 void main() {
-  test('test location deserialization', () {
-    const locationJSON =
-        '{ '
-        '"name": "Arashiyama Bamboo Grove", '
-        '"url": "https://cdn-images-1.medium.com/max/2000/1*vdJuSUKWl_SA9Lp-32ebnA.jpeg", '
-        '"facts": [{ '
-        '"title": "Summary", '
-        '"text": "This bamboo grove is on the outskirts of Kyoto." }] '
-        '}';
-    final locationMap = json.decode(locationJSON) as Map<String, dynamic>;
+  test('test /locations and /locations/:id', () async {
+    final locations = await Location.fetchAll();
+    for (var location in locations) {
+      expect(location.name, hasLength(greaterThan(0)));
+      expect(location.url, hasLength(greaterThan(0)));
 
-    expect('Arashiyama Bamboo Grove', equals(locationMap['name']));
-
-    final location = Location.fromJson(locationMap);
-
-    expect(location.name, equals(locationMap['name']));
-    expect(location.url, equals(locationMap['url']));
-
-    expect(location.facts[0].title, matches(locationMap['facts'][0]['title']));
-    expect(location.facts[0].text, matches(locationMap['facts'][0]['text']));
-
+      final fetchedLocation = await Location.fetchByID(location.id);
+      expect(fetchedLocation.name, equals(location.name));
+      expect(fetchedLocation.url, equals(location.url));
+      expect(fetchedLocation.facts, hasLength(location.facts.length));
+    }
   });
 }
